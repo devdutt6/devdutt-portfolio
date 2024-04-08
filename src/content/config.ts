ce054dfb2672e9e4c342926e73808f76ca1ba2ce
devdutt-evolution <devdutt.c@evolutioncloud.in>
2024-04-08 11:13:19 +0530

    weekend work

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,31 +1,31 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 
 const dateConfig: Intl.DateTimeFormatOptions = {
-  month: "short",
-  day: "2-digit",
-  year: "numeric",
+  month: 'short',
+  day: '2-digit',
+  year: 'numeric',
 };
-const dateFormat = "en-US";
+const dateFormat = 'en-US';
 
 // content for markdown, data for JSON or YAML
 const blogCollection = defineCollection({
-  type: "content",
+  type: 'content',
   schema: z.object({
-    title: z.string({ description: "Title of the page(H1)" }),
+    title: z.string({ description: 'Title of the page(H1)' }),
     og_title: z.string({
-      description: "Open-graph title of the article to show in previews",
+      description: 'Open-graph title of the article to show in previews',
     }),
     og_description: z.string({
-      description: "Open-graph description of the article to show in previews",
+      description: 'Open-graph description of the article to show in previews',
     }),
     og_image: z.string({
       description:
-        "Open-graph image of the article to show in previews, preferred aspect ratio 1.21:1",
+        'Open-graph image of the article to show in previews, preferred aspect ratio 1.21:1',
     }),
-    og_type: z.enum(["article", "website"]),
-    og_author: z.string({ description: "Name of the author" }),
+    og_type: z.enum(['article', 'website']),
+    og_author: z.string({ description: 'Name of the author' }),
     og_publishedOn: z
-      .string({ description: "Date in YYYY-MM-DD format" })
+      .string({ description: 'Date in YYYY-MM-DD format' })
       .default(Date.now().toString())
       .transform((date) =>
         new Date(date).toLocaleDateString(dateFormat, dateConfig)
@@ -34,6 +34,12 @@ const blogCollection = defineCollection({
   }),
 });
 
+const skillsCollection = defineCollection({
+  type: 'data',
+  schema: z.record(z.string(), z.array(z.string())),
+});
+
 export const collections = {
   blog: blogCollection,
+  skill: skillsCollection,
 };
