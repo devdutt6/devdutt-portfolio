600679a76f651fe2cd8ca1f86a662fafd3e72fd0
devdutt-evolution <devdutt.c@evolutioncloud.in>
2024-04-17 19:12:42 +0530

    fix: :construction: made javascript calls inline

--- src/scripts/aboutNavigator.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(`.clickEvent`).forEach((ele) => {
-  ele.addEventListener('click', () => {
-    scrollTo(ele.dataset.related);
-  });
-});
-function scrollTo(id) {
-  const scrollToElement = document.querySelector(`#${id}`);
-  scrollToElement?.scrollIntoView({ block: 'center', behavior: 'smooth' });
-}
