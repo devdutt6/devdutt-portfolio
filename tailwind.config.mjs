7290bdd8483adc9591b83c23f770685ea343140f
devdutt-evolution <devdutt.c@evolutioncloud.in>
2024-04-22 11:49:06 +0530

    style: :sparkles: fonts for page heading and project, blog title

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        headline: 'Platypi, serif',
+        pageTitle: 'Anton, sans-serif',
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
