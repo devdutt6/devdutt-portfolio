56f8bb419b2bafe13dc71bf9aa7d87380f021b56
Devduttsinh <devduttchudasama1339@gmail.com>
2024-04-28 11:50:13 +0530

    fix: :ambulance: for canonicalURLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  site: 'https://devdutt-portfolio.vercel.app',
+  integrations: [tailwind(), mdx()],
 });
